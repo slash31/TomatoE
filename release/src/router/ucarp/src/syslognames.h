fe635fcb391a6a135746065aadd62816f2ec13af
co12294 <20goto10@slash31.net>
2014-05-04 15:26:00 -0700

    Add source distributions for new features and update SSL

---
+++ release/src/router/ucarp/src/syslognames.h
@@ -0,0 +1,38 @@
+#ifndef __SYSLOGNAMES_H__
+#define __SYSLOGNAMES_H__ 1
+
+#ifdef SYSLOG_NAMESDISABLED
+
+struct {
+    const char *c_name;
+    int c_val;
+} facilitynames[] =
+  {
+    { "auth", LOG_AUTH },
+    { "authpriv", LOG_AUTHPRIV },
+    { "cron", LOG_CRON },
+    { "daemon", LOG_DAEMON },
+    { "ftp", LOG_FTP },
+    { "kern", LOG_KERN },
+    { "lpr", LOG_LPR },
+    { "mail", LOG_MAIL },
+    { "mark", INTERNAL_MARK },          /* INTERNAL */
+    { "news", LOG_NEWS },
+    { "security", LOG_AUTH },           /* DEPRECATED */
+    { "syslog", LOG_SYSLOG },
+    { "user", LOG_USER },
+    { "uucp", LOG_UUCP },
+    { "local0", LOG_LOCAL0 },
+    { "local1", LOG_LOCAL1 },
+    { "local2", LOG_LOCAL2 },
+    { "local3", LOG_LOCAL3 },
+    { "local4", LOG_LOCAL4 },
+    { "local5", LOG_LOCAL5 },
+    { "local6", LOG_LOCAL6 },
+    { "local7", LOG_LOCAL7 },
+    { NULL, -1 }
+  };
+
+#endif
+
+#endif
